505cea3d395e70bab2306e0205457fd935090fc5
Mohamed Ayman <mohameddallash21@gmail.com>
2024-04-23 01:43:19 +0200

    Refactor public key cryptosystem & Add signing and verification

--- PublicKeyCryptosystemRSA.py
+++ PublicKeyCryptosystemRSA.py
@@ -1,19 +1,80 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Signature import pss
 
-key = RSA.generate(1024)
-private_key = key
-public_key = key.public_key()
+class PublicKeyCryptosystemEncryption:
+    def __init__(self, public_key) -> None:
+        self.public_key = public_key
+        self.cipher_rsa = PKCS1_OAEP.new(public_key)
+
+    def encrypt(self, message):
+        return self.cipher_rsa.encrypt(message)
+
+class PublicKeyCryptosystemDecryption:
+    def __init__(self, private_key) -> None:
+        self.private_key = private_key
+        self.cipher_rsa = PKCS1_OAEP.new(private_key)
+
+    def decrypt(self, message):
+        return self.cipher_rsa.decrypt(message)
+
+class PublicKeyCryptosystemSigning:
+    def __init__(self, private_key) -> None:
+        self.private_key = private_key
+        self.signer = pss.new(private_key)
+
+    def sign(self, hashed_message):
+        return self.signer.sign(hashed_message)
+
+class PublicKeyCryptosystemVerification:
+    def __init__(self, public_key) -> None:
+        self.private_key = public_key
+        self.verifier = pss.new(public_key)
+
+    def verify(self, hashed_message, signature):
+        try:
+            self.verifier.verify(hashed_message,signature)
+            return True
+        except ValueError:
+            return False
 
 
-msg = b"Hello, this is a message to be encrypted"
-cipher_rsa = PKCS1_OAEP.new(public_key)
+if __name__ == '__main__':
+    private_key = RSA.generate(1024)
+    public_key = private_key.public_key()
 
-encrypted_msg = cipher_rsa.encrypt(msg)
+    encryption = PublicKeyCryptosystemEncryption(public_key)
 
-print('Encrypted: ', encrypted_msg)
+    ORIGINAL_MESSAGE = b"Hello, this is a message to be encrypted"
+    original_cipher_text = encryption.encrypt(ORIGINAL_MESSAGE)
+    # print(original_cipher_text)
 
-cipher_rsa = PKCS1_OAEP.new(private_key)
-decrypted_msg = cipher_rsa.decrypt(encrypted_msg)
+    TAMPERED_MESSAGE = b"This is a tampered message"
+    tampered_cipher_text = encryption.encrypt(TAMPERED_MESSAGE)
 
-print("Decrypted: ", decrypted_msg.decode())
+    from Crypto.Hash import SHA256
+    hash_sender = SHA256.new(ORIGINAL_MESSAGE)
+    signing = PublicKeyCryptosystemSigning(private_key)
+    signed_message = signing.sign(hash_sender)
+
+    # normal receive
+    decryption = PublicKeyCryptosystemDecryption(private_key)
+    plain_text = decryption.decrypt(original_cipher_text)
+    print(plain_text)
+    hash_receiver = SHA256.new(plain_text)
+    verifying = PublicKeyCryptosystemVerification(public_key)
+    if verifying.verify(hash_receiver,signed_message):
+        print("The message is original")
+    else:
+        print("The message have been tampered with")
+
+    # tampered receive
+    decryption = PublicKeyCryptosystemDecryption(private_key)
+    plain_text = decryption.decrypt(tampered_cipher_text)
+    print(plain_text)
+    hash_receiver = SHA256.new(plain_text)
+    verifying = PublicKeyCryptosystemVerification(public_key)
+    if verifying.verify(hash_receiver,signed_message):
+        print("The message is original")
+    else:
+        print("The message have been tampered with")
